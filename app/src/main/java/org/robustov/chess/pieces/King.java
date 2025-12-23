3e64d8db9fa9f693a50a6ecf025ab9182a0edaf7
robustov <yaroslavrobustov@yandex.ru>
2025-12-23 13:54:27 +0300

    added castling

--- app/src/main/java/org/robustov/chess/pieces/King.java
+++ app/src/main/java/org/robustov/chess/pieces/King.java
@@ -36,11 +36,67 @@ public class King extends Piece {
       }
     }
 
+    addCastlingMoves(position, board, validMoves);
+
     return validMoves;
   }
 
   private boolean isValidPosition(Position position) {
     return position.getFile() >= 'a' && position.getFile() <= 'h' &&
         position.getRank() >= 1 && position.getRank() <= 8;
+  }
+
+  private void addCastlingMoves(Position position, Board board, Set<Position> validMoves) {
+    if (hasMoved())
+      return;
+
+    if (canCastleKingside(position, board)) {
+      validMoves.add(new Position('g', position.getRank()));
+    }
+
+    if (canCastleQueenside(position, board)) {
+      validMoves.add(new Position('c', position.getRank()));
+    }
+  }
+
+  private boolean canCastleKingside(Position position, Board board) {
+    int rank = position.getRank();
+
+    Position fSquare = new Position('f', rank);
+    Position gSquare = new Position('g', rank);
+
+    if (board.hasPiece(fSquare) || board.hasPiece(gSquare)) {
+      return false;
+    }
+
+    Position rookPosition = new Position('h', rank);
+    if (!board.hasPiece(rookPosition) ||
+        !(board.getPiece(rookPosition).get() instanceof Rook rook) ||
+        rook.hasMoved()) {
+      return false;
+    }
+
+    return true;
+  }
+
+  private boolean canCastleQueenside(Position position, Board board) {
+    int rank = position.getRank();
+
+    Position bSquare = new Position('b', rank);
+    Position cSquare = new Position('c', rank);
+    Position dSquare = new Position('d', rank);
+
+    if (board.hasPiece(bSquare) || board.hasPiece(cSquare) || board.hasPiece(dSquare)) {
+      return false;
+    }
+
+    Position rookPosition = new Position('a', rank);
+    if (!board.hasPiece(rookPosition) ||
+        !(board.getPiece(rookPosition).get() instanceof Rook rook) ||
+        rook.hasMoved()) {
+      return false;
+    }
+
+    return true;
   }
 }
